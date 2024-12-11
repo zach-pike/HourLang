3703551d5819c9be7537ec88259b9dfb10b8cbdb
Zachary Pike <zachturnpike@gmail.com>
2024-12-11 07:29:31 -0500

    Add warning when file can't be opened

--- src/Utility/ReadFile.cpp
+++ src/Utility/ReadFile.cpp
@@ -3,6 +3,8 @@
 
 std::string ReadFile(std::filesystem::path path) {
     std::ifstream file(path);
+    if (!file.is_open()) throw std::runtime_error("Couldn't open file");
+
     std::stringstream ss;
     ss << file.rdbuf();
     return ss.str();
